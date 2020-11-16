4b4d1cbccecade1604ac45df6125e18350a498d5
adludo <a_tsai5@hotmail.com>
2020-11-17 00:05:59 +1100

    added image storage

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { API, Auth } from 'aws-amplify';
+import { API, Auth, Storage } from 'aws-amplify';
 import { listPosts } from './graphql/queries';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { v4 as uuid } from 'uuid';
 
 function App() {
 
-  const [posts, setPosts] = useState([])
-
+  const [images, setImages] = useState([]);
+  // const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    checkUser();
-    fetchPosts();
+    fetchImages();
+    // checkUser();
+    // fetchPosts();
   }, []);
 
-  async function checkUser() {
-    const user = await Auth.currentAuthenticatedUser();
-    console.log('user: ', user);
-    console.log('user attributes: ', user.attributes);
+  async function fetchImages() {
+    let s3images = await Storage.list('');
+    s3images = await Promise.all(s3images.map(async image => {
+      const signedImage = await Storage.get(image.key);
+      return signedImage;
+    }))
+    setImages(s3images);
   }
 
-  async function fetchPosts() {
-    try {
-      const postData = await API.graphql({ query: listPosts });
-      setPosts(postData.data.listPosts.items);
-      console.log('fetching posts')
-    } catch (err) {
-      console.log({ err })
-    }
+  function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    Storage.put(uuid(), file).then (() => fetchImages())
   }
+
+  // async function checkUser() {
+  //   const user = await Auth.currentAuthenticatedUser();
+  //   console.log('user: ', user);
+  //   console.log('user attributes: ', user.attributes);
+  // }
+
+  // async function fetchPosts() {
+  //   try {
+  //     const postData = await API.graphql({ query: listPosts });
+  //     setPosts(postData.data.listPosts.items);
+  //     console.log('fetching posts')
+  //   } catch (err) {
+  //     console.log({ err })
+  //   }
+  // }
 
   return (
     <div>
-      <h1> Hello world! </h1>
+      {/* <h1> Hello world! </h1>
       {
         posts.map(post => (
           <div key={post.id}>
@@ -40,11 +57,21 @@ function App() {
             <p>{post.location}</p>
           </div>
         ))
-      }
+      } */}
+      <h1>Photo Album</h1>
+      <span>Add new image</span>
+      <input
+        type="file"
+        accept='image/png'
+        onChange={onChange}
+      />
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        { images.map(image => <img src={image} style={{width: 400, marginBottom :10}}/>)}
+      </div>
       <AmplifySignOut />
     </div>
-  )
 
+  )
 };
 
 export default withAuthenticator(App);
