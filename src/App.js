a783ce7a0d9eb3bdb0868c89c4a7b3875fcb43fd
adludo <a_tsai5@hotmail.com>
2020-11-20 00:32:18 +1100

    coverted app into router in App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import {
+  HashRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import { css } from '@emotion/css';
 import { API, Auth, Storage } from 'aws-amplify';
 import { listPosts } from './graphql/queries';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { v4 as uuid } from 'uuid';
 
 import Button from './Button';
+import Posts from './Posts';
+import Post from './Posts';
+import Header from './Header';
+import CreatePost from './CreatePost';
+import { updatePost } from './graphql/mutations';
 
-function App() {
+// Changing 'App' to 'Router'
+function Router() {
+  // create pieces of initial state
   const [showOverlay, updateOverlayVisibility] = useState(false);
-  const [images, setImages] = useState([]);
+  const [posts, updatePosts] = useState([]);
+  // const [images, setImages] = useState([]);
   // const [posts, setPosts] = useState([]);
 
+  // fetch stuff when component loads
   useEffect(() => {
-    fetchImages();
+    //fetchImages();
     // checkUser();
-    // fetchPosts();
+    fetchPosts();
   }, []);
 
-  async function fetchImages() {
-    let s3images = await Storage.list('');
-    s3images = await Promise.all(s3images.map(async image => {
-      const signedImage = await Storage.get(image.key);
-      return signedImage;
-    }))
-    setImages(s3images);
-  }
+  // async function fetchImages() {
+  //   let s3images = await Storage.list('');
+  //   s3images = await Promise.all(s3images.map(async image => {
+  //     const signedImage = await Storage.get(image.key);
+  //     return signedImage;
+  //   }))
+  //   setImages(s3images);
+  // }
 
-  function onChange(e) {
-    if (!e.target.files[0]) return
-    const file = e.target.files[0];
-    Storage.put(uuid(), file).then (() => fetchImages())
-  }
+  // function onChange(e) {
+  //   if (!e.target.files[0]) return
+  //   const file = e.target.files[0];
+  //   Storage.put(uuid(), file).then (() => fetchImages())
+  // }
 
   // async function checkUser() {
   //   const user = await Auth.currentAuthenticatedUser();
@@ -39,42 +54,93 @@ function App() {
   //   console.log('user attributes: ', user.attributes);
   // }
 
-  // async function fetchPosts() {
-  //   try {
-  //     const postData = await API.graphql({ query: listPosts });
-  //     setPosts(postData.data.listPosts.items);
-  //     console.log('fetching posts')
-  //   } catch (err) {
-  //     console.log({ err })
-  //   }
-  // }
+  async function fetchPosts() {
+    try {
+      // query API, ask for 100 items?
+      let postData = await API.graphql({ query: listPosts, variables: { limit: 100 }});
+      let postsArray = postData.data.listPosts.items;
+      // const postData = await API.graphql({ query: listPosts });
+
+      // mapover the image keys in posts array, get signed image urls for each image
+      postsArray = await Promise.all(postsArray.map(async post => {
+        const imageKey = await Storage.get(post.image);
+        post.image = imageKey;
+        return post;
+      }));
+      // update the posts array in the local state
+      setPostState(postsArray);
+      // setPosts(postData.data.listPosts.items);
+      console.log('fetching posts')
+    } catch (err) {
+      console.log({ err })
+    }
+
+    async function setPostState(postsArray) {
+      updatePosts(postsArray);
+    }
+  }
 
   return (
-    <div>
-      {/* <h1> Hello world! </h1>
-      {
-        posts.map(post => (
-          <div key={post.id}>
-            <h3>{post.name}</h3>
-            <p>{post.location}</p>
-          </div>
-        ))
-      } */}
-      <h1>Photo Album</h1>
-      <span>Add new image</span>
-      <Button title="New Post" onClick={()=> updateOverlayVisibility(true)}/>
-      <input
-        type="file"
-        accept='image/png'
-        onChange={onChange}
-      />
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        { images.map(image => <img src={image} style={{width: 400, marginBottom :10}}/>)}
-      </div>
-      <AmplifySignOut />
-    </div>
+    <>
+      <HashRouter>
+        <div className={contentStyle}>
+          <Header />
+          <hr className={dividerStyle} />
+          <Button title="New Post" onClick={() => updatePost(true)} />
+          <Switch>
+            <Route exact path="/" >
+              <Posts posts={posts}/>
+            </Route>
+            <Route path="/post/:id">
+              <Post />
+            </Route>
+          </Switch>
+        </div>
+        <AmplifySignOut />
+      </HashRouter>
+      { showOverlay && (
+        <CreatePost 
+          updateOverlayVisibility={updateOverlayVisibility}
+          updatePosts={setPostState}
+          posts={posts}
+        />
+      )}
+    </>
+    // <div>
+    //   {/* <h1> Hello world! </h1>
+    //   {
+    //     posts.map(post => (
+    //       <div key={post.id}>
+    //         <h3>{post.name}</h3>
+    //         <p>{post.location}</p>
+    //       </div>
+    //     ))
+    //   } */}
+    //   <h1>Photo Album</h1>
+    //   <span>Add new image</span>
+    //   <Button title="New Post" onClick={()=> updateOverlayVisibility(true)}/>
+    //   <input
+    //     type="file"
+    //     accept='image/png'
+    //     onChange={onChange}
+    //   />
+    //   <div style={{display: 'flex', flexDirection: 'column'}}>
+    //     { images.map(image => <img src={image} style={{width: 400, marginBottom :10}}/>)}
+    //   </div>
+    //   <AmplifySignOut />
+    // </div>
 
-  )
+  );
 };
 
-export default withAuthenticator(App);
+const dividerStyle = css`
+  margin-top: 15px;
+`
+
+const contentStyle = css`
+  min-height: calc(100vh - 45px);
+  padding: 0px 40px;
+`
+
+export default withAuthenticator(Router);
+
